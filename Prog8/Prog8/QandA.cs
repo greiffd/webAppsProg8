a4033f98f49342d9869141c61bbc7226710a4b65
David Greiff <greiffd@uwplatt.edu>
2020-11-25 22:37:36 -0600

    added Prog8 project

---
+++ Prog8/Prog8/QandA.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Prog8
+{
+    public class QandA : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void DisplayQuestion()
+        {
+            // Call the broadcastMessage method to update clients with a random question from the database.
+            var rand = new Random();
+            var id = rand.Next(1, 18);
+
+            string msg = "";
+
+            /*
+            * Connects to database and inserts data into gridview
+            */
+            string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(cs);
+
+            string command = "select Question from Questions where id =" + id;
+            SqlDataAdapter adapt = new SqlDataAdapter(command, con);
+            con.Open();
+            adapt.Fill(dt);
+
+            // Assign the question to msg
+            msg = dt.Rows[0][0].ToString();
+            con.Close();
+
+            Clients.All.broadcastMessage(msg);
+        }
+    }
+}
---
+++ Prog8/Prog8/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Prog8.Startup))]
+
+namespace Prog8
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
